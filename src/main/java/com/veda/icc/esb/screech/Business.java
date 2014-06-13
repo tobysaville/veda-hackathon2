ec576cad4bf9faf1803ada67db2be6b78ceb5f4c
toby.saville <toby.saville@veda.com.au>
2014-06-13 04:34:31 +0000

    added beanio example code

--- src/main/java/com/veda/icc/esb/screech/Business.java
+++ src/main/java/com/veda/icc/esb/screech/Business.java
@@ -1,5 +1,20 @@
 package com.veda.icc.esb.screech;
 
-public class Business {
+import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "CommercialPlusConsumerCreditEnquiryByFileNumberRequest", namespace = "http://www.vedaxml.com/bureau/internal/individual/v1")
+@XmlType(namespace="http://www.vedaxml.com/bureau/internal/individual/v1")
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class Business implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @XmlElement
+    private String id;
 }
